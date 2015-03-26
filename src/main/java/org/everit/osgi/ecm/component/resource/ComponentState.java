736e2f836363c1f58ef6af06871939e3d854724b
Balazs Zsoldos <balazs.zsoldos@everit.biz>
2015-03-26 10:53:31 +0100

    Upgrading javadoc in ComponentState

--- src/main/java/org/everit/osgi/ecm/component/resource/ComponentState.java
+++ src/main/java/org/everit/osgi/ecm/component/resource/ComponentState.java
@@ -15,6 +15,8 @@
  */
 package org.everit.osgi.ecm.component.resource;
 
+import org.omg.PortableInterceptor.ACTIVE;
+
 /**
  * States of the lifecycle of a component instance.
  */
@@ -28,29 +30,45 @@ public enum ComponentState {
 
   /**
    * A component is in the failed state if any of the setter the activate or the update method
-   * throws a runtime exception. Deactivate method will not be called if the component fails during
-   * calling setters, activate or update method.
+   * throws a runtime exception or a configuration cannot be applyed due to multiplicity or type
+   * issues.
    */
   FAILED,
 
   /**
-   * The component will be permanently failed if implementation change is necessary to make it work.
+   * The component will be permanently failed if implementation (or metadata) change is necessary to
+   * make it work.
    */
   FAILED_PERMANENT,
 
   /**
-   * The activate method of the component is currently processing.
+   * The component is not yet initialized or it was removed. It is hard to catch this state as
+   * <ul>
+   * <li>As soon as the component context is created, initialization starts.</li>
+   * <li>As soon as the component context is inactivated, it is removed from the system completely
+   * (as this happens when the container is closed).</li>
+   * </ul>
+   */
+  INACTIVE,
+
+  /**
+   * The component is instantiated and the setters or the activate method of the component is
+   * currently processing.
    */
   STARTING,
 
   /**
-   * The component is stopped. This state cannot really be coughed as the component instance is
-   * deleted as soon as it is in the STOPPED state.
-   */
-  STOPPED,
-
-  /**
-   * The deactivate function of the component is currently running.
+   * The deactivate function of the component is currently running. {@link STOPPING} can be caused
+   * due to several reasons:
+   * <ul>
+   * <li>The component is destroyed as the configuration behind the component is deleted</li>
+   * <li>Due to a non-dynamic configuration update, the component has to be restarted. In this case
+   * the first step is stopping the component.</li>
+   * <li>The component was in {@link STARTING} or {@link ACTIVE} state and there was a failure. In
+   * the end of STOPPING, the component will get the {@link #FAILED} or {@linkg #FAILED_PERMANENT}
+   * state.</li>
+   * <li>The component was {@link ACTIVE} and one of the references became unsatisfied.</li>
+   * </ul>
    */
   STOPPING,
 
@@ -60,9 +78,14 @@ public enum ComponentState {
   UNSATISFIED,
 
   /**
-   * The component gets the UPDATE_CONFIGURATION state if the component has not been ACTIVE and new
-   * configuration arrived, or the component was active but it had to be stopped to apply
-   * non-dynamic changes.
+   * The component instance is just created and new configuration is applied on it. This component
+   * can get this state due to the following reasons:
+   * <ul>
+   * <li>the component was {@link FAILED} and new configuration arrived</li>
+   * <li>the component was {@link UNSATISFIED} and new configuration arrived</li>
+   * <li>the component was {@link ACTIVE} but it had to be stopped to apply non-dynamic changes. In
+   * this case the component class is re-instantiated.</li>
+   * </ul>
    */
   UPDATING_CONFIGURATION
 }
